ab771c1a4f7eba16485e83a590a1d5abafe680c6
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-07-17 09:59:40 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);              //true
+// console.log(2 >= 1);             //true
+// console.log(2 < 1);              //false
+// console.log(2 <= 1);             //false
+// console.log(2 == 1);             //false
+// console.log(2 != 1);             //true
+
+// console.log("2" > 1);         // it will automaticall convert into number
+// console.log("02" > 1);
+// console.log("2" > 4);
+
+// console.log(null > 0);          //false
+// console.log(null < 0);          //false
+// console.log(null == 0);         //false
+// console.log(null >= 0);         //true
+
+// console.log(undefined > 0);          //false
+// console.log(undefined < 0);          //false
+// console.log(undefined == 0);         //false
+// console.log(undefined >= 0);         //false
+
+
+/*Strict check => ===  (it will check the value as well as its dataType also)*/
+console.log("2"==2);   //true [normal check which checked only the value.]
+console.log("2"===2);  //false (it will check the value as well as its dataType also)
+
