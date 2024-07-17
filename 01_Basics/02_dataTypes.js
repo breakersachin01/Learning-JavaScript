bbfc971bd3b6393b6fb72d6ef4a868bc658c9065
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-07-17 08:24:30 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";    // treat all JS code as newer version
+
+// alert(3+3)    // we are using nodejs, not browser
+
+console.log(3
+    +
+    3)          // code readability should be high
+console.log("Sachin")
+
+let name = "ashish"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// DataTypes:
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// Boolean => true/false
+// null => standalone value
+// undefined => it comes when value is not assigned.
+// symbol => for uniqueness
+
+//Object
+
+console.log(typeof age);              //it will show you the variable Datatype
+console.log(typeof undefined);        // undefined datatype
+console.log(typeof null);             // null is the Object DataType
