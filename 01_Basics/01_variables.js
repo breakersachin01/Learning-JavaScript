d1e7ac665df8bc431ef19645e3c217684d9fedc3
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-07-17 08:23:00 +0000

    varibales

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 13423                  // contant varibale can't be changable
+let accountEmail = "sachin@gmail.com"    //'let' variable is only available within the block (e.g., a function, loop, or conditional).
+var accountPassword = "12345"            // variables of string type
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 3               // not allowed
+accountEmail = "ashish@gmail.com"
+accountPassword = 9887
+accountCity = "Bengaluru"
+
+// console.log(accountId);         // it is used for single output
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])             // it print the multiple value in form of tables.
