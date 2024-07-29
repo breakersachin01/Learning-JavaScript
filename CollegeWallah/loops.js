61aa3448c06764bfee48c5144e3f6c4070e3658d
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-07-29 16:50:39 +0000

    loops

---
+++ CollegeWallah/loops.js
@@ -0,0 +1,21 @@
+///for loop
+
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+///while loop
+
+// let i = 1;
+// while(i<=15){
+//     console.log(i);
+//     i++;
+// }
+
+///do while loop         ===> it is execute once if the condition is not match.
+
+let i = 1;
+do{
+    console.log(i);
+    i++;
+}while(i<=15);
