dd77fcfa74e03f8cad0b74ed139386c2aefc9931
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-07-29 16:59:14 +0000

    loops pattern printing

--- CollegeWallah/loops.js
+++ CollegeWallah/loops.js
@@ -14,8 +14,17 @@
 
 ///do while loop         ===> it is execute once if the condition is not match.
 
-let i = 1;
-do{
-    console.log(i);
-    i++;
-}while(i<=15);
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=15);
+
+
+/*Pattern Printing */
+
+for(let i = 1; i<=5; i++){
+    for(let j=1; j<=i; j++){
+        console.log(i);                 //But it will all print in the next line so we need to store the element in anything the print it.(baki sab same hai concept pattern printing like in other language)
+    }
+}
