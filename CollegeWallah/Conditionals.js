8332f0d46c1ce2fe0ae47041c4f8bd9d55007b9e
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-07-29 16:36:41 +0000

    conditions by college Wallah

---
+++ CollegeWallah/Conditionals.js
@@ -0,0 +1,42 @@
+age  = 31;
+/*can Vote */
+// if (age >= 18){
+//     console.log("Adult");
+// }
+// else{
+//     console.log("child");
+// }
+
+/*Even Odd */
+// if (age%2 == 0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+
+/*Ratings */
+// rating = 2;
+// /*
+// // 1 - Poor
+// // 2 - Below Average
+// // 3 - Average
+// // 4 - Good
+// // 5 - Excellent
+// */
+// if(rating==1) console.log("Poor");
+// else if(rating==2) console.log("Below Average");
+// else if(rating==3) console.log("Average");
+// else if(rating==4) console.log("Good");
+// else console.log("Excellent");                               //rating ==> 5
+
+
+/*check Three digit Number */
+num = 28;
+if(num>=100 && num<=999){
+    console.log("Yes, Three digit Number");
+}
+else{
+    console.log("No it's Not");
+}
+
