7590d2c999476239a972a50939d173ac0af2e269
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-07-29 18:28:28 +0000

    arrays

--- CollegeWallah/03arrays.js
+++ CollegeWallah/03arrays.js
@@ -31,9 +31,56 @@
 // // console.log(arr);
 
 /*But if we declared the varible as 'let' so we can do it easily like*/
-let arr = [2,3,4,5];
-console.log(arr);
-arr[2]=9;
-console.log(arr);
-arr  = 2.5;         
-console.log(arr)
+// let arr = [2,3,4,5];
+// console.log(arr);
+// arr[2]=9;
+// console.log(arr);
+// arr  = 2.5;         
+// console.log(arr)
+
+
+
+//We can add anytype of data in the arrays of javascript
+// const arr = [2,"javed",7.87,true];
+// console.log(arr);
+// arr.push("sachin");
+// console.log(arr);
+// arr.push(9.45);
+// console.log(arr);
+// arr.push(20/2);
+// console.log(arr);
+
+
+/**************2D-Arrays(Note: in any programing languague we can't create different size of arrays but in the JavaScript  we can different types of length arrays)*/
+// arr = [[1,2,3,4],[9,8,7,6]];           //==>> both have the same length of arrays which is valid in any programming language of Arrays.
+
+// arr = [[1,2,3,4],[9,8,7,6,5,10]];         //==>> both are the different length of arrays which is Not valid in any programming language of Arrays EXCEPT JavaScript it is valid in it.
+
+// console.log(arr);
+
+
+
+/*****Print of arrays Iteratively*/
+arr = [9,8,7,6,5,4];
+// console.log(typeof(arr));               //Object
+
+//1>> for loop:--      it will print the exact value of the element of the arrays.
+// for(let i = 0;i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+//2>> forOf loop:----  it will print the copy value of the element of the arrays by storing into new varible.
+// for (const iterator of arr) {
+//     console.log(iterator);
+// }
+// console.log(arr);
+
+//3>> forEach loop:----  basically it is used to for the puspose of index of the element.
+arr.forEach((ele,i,arr) => {              //we can pass the (elementvalue, index of the element, arr) together or we can pass it individualy also
+    // console.log(ele);                     //print element of the arrays.
+    // console.log(i);                     //print index of the element of the arrays.
+    // console.log(arr);                     //print array till loops.
+    // console.log(ele,i);                     //print element and index of that element of the arrays.
+    console.log(ele,i,arr);                     //print element and index and the array of the arrays.
+ 
+});
