9781457aeaf3bc47c66f10cd0adb1ca73c7a8f4b
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-07-31 19:11:59 +0000

    templateLiteral

---
+++ CollegeWallah/06templateLiteral.js
@@ -0,0 +1,11 @@
+let age = 30;
+let exp = 10;
+
+// console.log("My age is "+age+" and My experience is "+exp);      //actually its work but this is the not right way
+console.log(`My age is ${age} and my experience is ${exp}`);        // this the most useful way (called backtick (``))
+
+// print string ==> sac"hin
+console.log("sac"+'"'+"hin");   // using single cote inner
+console.log('sac"hin');         // using single cote outer
+console.log("sac\"hin");        // using scape-sequence (\);
+console.log(`sac"hin`);         // using backtik; most usable
