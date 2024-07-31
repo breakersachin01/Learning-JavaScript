806895958b931ad512df37e89f11de390e86630b
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-07-31 18:07:59 +0000

    truthy falsy

---
+++ CollegeWallah/04truthyFalsy.js
@@ -0,0 +1,20 @@
+/*Falsy values --> (false condition)=> false, 0, null, NaN(not a number), undefiend
+//truthy values--> (true condition)=> everthing except falsy values.
+*/ 
+
+// if(5>3){
+//     console.log("Hello");       //truthy value
+// }
+// else console.log("mello");      // falsy value
+
+// //falsy values---->>
+// if(false){                 // false, 0, null, NaN, undifiend ==>> then print falsy value.
+//     console.log("Hello");       //truthy value
+// }
+// else console.log("mello");      // falsy value
+
+//truthy values---->>
+if('s'){                 // everthing except falsy value ex:- 'a', "data", 3, etc 
+    console.log("Hello");       //truthy value
+}
+else console.log("mello");      // falsy value
