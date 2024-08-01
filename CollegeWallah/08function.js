a2945abde1ca35d254af474e560f37a2f3a28d30
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-08-01 05:57:31 +0000

    funtion

---
+++ CollegeWallah/08function.js
@@ -0,0 +1,42 @@
+// function OnetoN(n){             //create funtion
+//     for(let i=1;i<=n;i++){
+//         console.log(i);
+//     }
+// }
+// OnetoN(10);           // call function
+// console.log();
+// OnetoN(6);
+
+
+// let a = -2;
+// let b = 3;
+// // console.log(Math.abs(a*a*a)+Math.abs(b*b*b));          //Math.abs convert -ve value into absolute value.
+
+
+// function Qsum(a,b){
+//     return Math.abs(a*a*a)+Math.abs(b*b*b);
+// }
+// console.log(Qsum(-2,3));
+
+
+
+
+
+// ----------------------MathematicalBuildIn funtion-------------------------
+
+// console.log(Math.abs(-6));
+// console.log(Math.max(5,1));
+// console.log(Math.min(-5,2));
+// console.log(Math.max(23,5,-2,9,29,52)); //in  Java we can only able to give two agruments but IN JavaScript we can give multiple arguments
+// console.log(Math.pow(2,3));
+// console.log(Math.pow(2.5,3.7));
+// console.log(Math.sqrt(144));               //give the Squareroot of given number.
+// console.log(Math.cbrt(27));                //gives the cuberoot of given number
+// console.log(Math.log10(10000));               //it give the log10 value.
+// console.log(Math.floor(5.9));
+// console.log(Math.ceil(4.1));
+console.log(Math.random());              //Math.random() generates a random real number between 0 and 1 basically used to generate the otp.
+console.log(Math.random()*10000);
+console.log(Math.trunc(Math.random()*10000));         //trunc is used to print the integer part only.
+
+console.log(Math.floor(Math.random()*10000));
