706fd55426b7f7c023c706a2f15043bee74a8205
Sachin Vishwakarma <120857189+breakersachin01@users.noreply.github.com>
2024-08-03 05:29:57 +0000

    map

--- CollegeWallah/11MapFilterReduce.js
+++ CollegeWallah/11MapFilterReduce.js
@@ -63,9 +63,9 @@ let arr = [1,9,2,7,4,5,6,2,8];
 // console.log(arr);
 
 let x = arr.reduce(function(a,b){
-    // return a+b;
+    return a+b;
     // return a-b;
-    return a*b
+    // return a*b
 });
 console.log(x);
 
